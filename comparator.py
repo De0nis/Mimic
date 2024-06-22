fef0ce38080ce21c39be801676e4322a3ee933c3
M. Denis <96110531+De0nis@users.noreply.github.com>
2024-06-22 15:57:13 +0700

    Update comparator.py

--- comparator.py
+++ comparator.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageChops
 import cv2
 
 def make_screenshot(screenshot_no,x,y):
-        screenshot = pyautogui.screenshot(region=(x-50, y-50, x+50,y+50))
+        screenshot = pyautogui.screenshot(region=((x-50),(y-50), 100, 100))
         screenshot.save(f'./screenshots/{screenshot_no}_.png')
         sample = os.path.abspath(f"./screenshots/{screenshot_no}.png")
         img1 = Image.open(f'./screenshots/{screenshot_no}_.png')
@@ -20,4 +20,4 @@ def make_screenshot(screenshot_no,x,y):
         sum_diff_area=0
         for cnt in contours:
                 sum_diff_area+=cv2.contourArea(cnt)
-        print(f'Average difference: {sum_diff_area}')
+        return sum_diff_area
